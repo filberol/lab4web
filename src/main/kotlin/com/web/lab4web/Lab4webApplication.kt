9974a51a8894adb0a99f3ea463beffc4af3f42fc
filberol <mkh.vadim@gmail.com>
2022-11-27 13:52:12 +0300

    Initial commit, spring boot + react js setup.

---
+++ src/main/kotlin/com/web/lab4web/Lab4webApplication.kt
@@ -0,0 +1,11 @@
+package com.web.lab4web
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab4webApplication
+
+fun main(args: Array<String>) {
+    runApplication<Lab4webApplication>(*args)
+}
---
+++ webapp/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      Здесь пока ничего нет.
+    </div>
+  );
+}
+
+export default App;
